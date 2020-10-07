234627401b98dc2798ce2b45e31a1993af8556fa
Timo Klimmer <timo.klimmer@microsoft.com>
2020-10-07 19:07:59 +0200

    Added registration of ADLS Gen2 and Blob Storage

--- foundation/setup/azureml/setup.py
+++ foundation/setup/azureml/setup.py
@@ -1,4 +1,4 @@
-from azureml.core import Workspace
+from azureml.core import Workspace, Datastore
 from azureml.core.compute import AksCompute, AmlCompute, ComputeTarget
 from azureml.exceptions import ComputeTargetException
 from environs import Env
@@ -19,10 +19,27 @@ workspace_region = env("WORKSPACE_REGION")
 workspace_hbi = env.bool("WORKSPACE_HBI")
 
 # - service principals
+# note: SP credentials for data store are load below with the data store infos
 # SP for deploying a model as webservice
 deploy_model_sp_tenant_id = env("DEPLOY_MODEL_SP_TENANT_ID")
 deploy_model_sp_app_id = env("DEPLOY_MODEL_SP_APP_ID")
 deploy_model_sp_secret = env("DEPLOY_MODEL_SP_SECRET")
+
+# - data stores
+# SOME_EXTERNAL_ADLS_GEN2_DATASTORE
+some_external_adls_gen2_datastore_enabled = env.bool("SOME_EXTERNAL_ADLS_GEN2_DATASTORE_ENABLED")
+some_external_adls_gen2_datastore_name = env("SOME_EXTERNAL_ADLS_GEN2_DATASTORE_NAME")
+some_external_adls_gen2_datastore_account_name = env("SOME_EXTERNAL_ADLS_GEN2_DATASTORE_ACCOUNT_NAME")
+some_external_adls_gen2_datastore_file_system = env("SOME_EXTERNAL_ADLS_GEN2_DATASTORE_FILE_SYSTEM")
+some_external_adls_gen2_datastore_sp_tenant_id = env("SOME_EXTERNAL_ADLS_GEN2_DATASTORE_SP_TENANT_ID")
+some_external_adls_gen2_datastore_sp_app_id = env("SOME_EXTERNAL_ADLS_GEN2_DATASTORE_SP_APP_ID")
+some_external_adls_gen2_datastore_sp_password = env("SOME_EXTERNAL_ADLS_GEN2_DATASTORE_SP_PASSWORD")
+# SOME_EXTERNAL_BLOB_DATASTORE
+some_external_blob_datastore_enabled = env.bool("SOME_EXTERNAL_BLOB_DATASTORE_ENABLED")
+some_external_blob_datastore_name = env("SOME_EXTERNAL_BLOB_DATASTORE_NAME")
+some_external_blob_container_name = env("SOME_EXTERNAL_BLOB_CONTAINER_NAME")
+some_external_blob_account_name = env("SOME_EXTERNAL_BLOB_ACCOUNT_NAME")
+some_external_blob_account_key = env("SOME_EXTERNAL_BLOB_ACCOUNT_KEY")
 
 # - clusters
 # CPU batch cluster
@@ -30,17 +47,13 @@ cpu_batch_cluster_name = env("CPU_BATCH_CLUSTER_NAME")
 cpu_batch_cluster_vm_size = env("CPU_BATCH_CLUSTER_VM_SIZE")
 cpu_batch_cluster_min_nodes = env.int("CPU_BATCH_CLUSTER_MIN_NODES")
 cpu_batch_cluster_max_nodes = env.int("CPU_BATCH_CLUSTER_MAX_NODES")
-cpu_batch_cluster_idle_seconds_before_scaledown = env.int(
-    "CPU_BATCH_CLUSTER_IDLE_SECONDS_BEFORE_SCALEDOWN"
-)
+cpu_batch_cluster_idle_seconds_before_scaledown = env.int("CPU_BATCH_CLUSTER_IDLE_SECONDS_BEFORE_SCALEDOWN")
 # GPU batch cluster
 gpu_batch_cluster_name = env("GPU_BATCH_CLUSTER_NAME")
 gpu_batch_cluster_vm_size = env("GPU_BATCH_CLUSTER_VM_SIZE")
 gpu_batch_cluster_min_nodes = env.int("GPU_BATCH_CLUSTER_MIN_NODES")
 gpu_batch_cluster_max_nodes = env.int("GPU_BATCH_CLUSTER_MAX_NODES")
-gpu_batch_cluster_idle_seconds_before_scaledown = env.int(
-    "GPU_BATCH_CLUSTER_IDLE_SECONDS_BEFORE_SCALEDOWN"
-)
+gpu_batch_cluster_idle_seconds_before_scaledown = env.int("GPU_BATCH_CLUSTER_IDLE_SECONDS_BEFORE_SCALEDOWN")
 # AKS Cluster
 # enabled
 aks_cluster_enabled = env.bool("AKS_CLUSTER_ENABLED")
@@ -50,9 +63,7 @@ aks_cluster_region = env("AKS_CLUSTER_REGION")
 aks_cluster_agent_count = env.int("AKS_CLUSTER_AGENT_COUNT")
 aks_cluster_vm_size = env("AKS_CLUSTER_VM_SIZE")
 # SSL/TLS
-aks_cluster_use_certificate_from_microsoft = env.bool(
-    "AKS_CLUSTER_USE_CERTIFICATE_FROM_MICROSOFT"
-)
+aks_cluster_use_certificate_from_microsoft = env.bool("AKS_CLUSTER_USE_CERTIFICATE_FROM_MICROSOFT")
 aks_cluster_leaf_domain_label = env("AKS_CLUSTER_LEAF_DOMAIN_LABEL", None)
 # custom certificate
 # notes: - no values needed if a certificate from Microsoft is used
@@ -100,15 +111,41 @@ keyvault.set_secret(name="DEPLOY-MODEL-SP-TENANT-ID", value=deploy_model_sp_tena
 keyvault.set_secret(name="DEPLOY-MODEL-SP-APP-ID", value=deploy_model_sp_app_id)
 keyvault.set_secret(name="DEPLOY-MODEL-SP-SECRET", value=deploy_model_sp_secret)
 
+# data stores
+print("Registering data stores...")
+# SOME_EXTERNAL_ADLS_GEN2_DATASTORE_ENABLED
+if some_external_adls_gen2_datastore_enabled:
+    print(f"- {some_external_adls_gen2_datastore_name}...")
+    Datastore.register_azure_data_lake_gen2(
+        workspace=workspace,
+        datastore_name=some_external_adls_gen2_datastore_name,
+        filesystem=some_external_adls_gen2_datastore_file_system,
+        account_name=some_external_adls_gen2_datastore_account_name,
+        tenant_id=some_external_adls_gen2_datastore_sp_tenant_id,
+        client_id=some_external_adls_gen2_datastore_sp_app_id,
+        client_secret=some_external_adls_gen2_datastore_sp_password,
+    )
+# SOME_EXTERNAL_BLOB_DATASTORE
+if some_external_blob_datastore_enabled:
+    print(f"- {some_external_blob_datastore_name}...")
+    Datastore.register_azure_blob_container(
+        workspace=workspace,
+        datastore_name=some_external_blob_datastore_name,
+        container_name=some_external_blob_container_name,
+        account_name=some_external_blob_account_name,
+        account_key=some_external_blob_account_key,
+    )
+
+
 # environment(s)
 # TODO: complete as needed
 
+
+# clusters
 # CPU batch cluster
 print("Setting up CPU batch compute cluster...")
 try:
-    cpu_batch_compute_cluster = ComputeTarget(
-        workspace=workspace, name=cpu_batch_cluster_name
-    )
+    cpu_batch_compute_cluster = ComputeTarget(workspace=workspace, name=cpu_batch_cluster_name)
     print(f"Cluster '{cpu_batch_cluster_name}' exists already.")
     # TODO: add cluster update (once needed)
 except ComputeTargetException:
@@ -129,9 +166,7 @@ except ComputeTargetException:
 # GPU batch cluster
 print("Setting up GPU batch compute cluster...")
 try:
-    gpu_batch_compute_cluster = ComputeTarget(
-        workspace=workspace, name=gpu_batch_cluster_name
-    )
+    gpu_batch_compute_cluster = ComputeTarget(workspace=workspace, name=gpu_batch_cluster_name)
     print(f"Cluster '{gpu_batch_cluster_name}' exists already.")
     # TODO: add cluster update (once needed)
 except ComputeTargetException:
@@ -175,9 +210,7 @@ if aks_cluster_enabled:
         )
 
         if aks_cluster_use_certificate_from_microsoft:
-            provisioning_configuration.enable_ssl(
-                leaf_domain_label=aks_cluster_leaf_domain_label
-            )
+            provisioning_configuration.enable_ssl(leaf_domain_label=aks_cluster_leaf_domain_label)
         else:
             provisioning_configuration.enable_ssl(
                 ssl_cert_pem_file=aks_cluster_ssl_cert_pem_file,
@@ -192,18 +225,5 @@ if aks_cluster_enabled:
         )
 
         aks_cluster.wait_for_completion(show_output=True)
-
-# additional general data store used by all projects
-# some_external_blob_datastore_name = env("SOME_EXTERNAL_BLOB_DATASTORE_NAME"]
-# some_external_blob_container_name = env("SOME_EXTERNAL_BLOB_CONTAINER_NAME"]
-# some_external_blob_account_name = env("SOME_EXTERNAL_BLOB_ACCOUNT_NAME"]
-# some_external_blob_account_key = env("SOME_EXTERNAL_BLOB_ACCOUNT_KEY"]
-# Datastore.register_azure_blob_container(
-#     workspace=workspace,
-#     datastore_name=some_external_blob_datastore_name,
-#     container_name=some_external_blob_container_name,
-#     account_name=some_external_blob_account_name,
-#     account_key=some_external_blob_account_key,
-# )
 
 print("Done.")
